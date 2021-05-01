3230389be50fa38f2d443973d80d8a79f86bcb55
Tung Pham <tung.42@gmail.com>
2021-05-01 12:00:01 +0700

    Update index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,113 +15,223 @@ router.param("fen", function(req, res, next, fen){
 router.get("/", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
   res.render("pages/ai", {
+    bodyClass: "home",
+    level: 3,
+    levelTxt: "Bình thường",
+    fullUrl: fullUrl,
+    title: "Trang chủ",
+    board: "",
+    langUrl: "/en"
+  });
+});
+
+router.get("/gioi-thieu", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/about", {
+    bodyClass: "about",
+    fullUrl: fullUrl,
+    title: "Giới thiệu",
+    board: "",
+    langUrl: "/about"
+  });
+});
+
+router.get("/moi-choi", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/ai", {
     bodyClass: "game",
+    level: 1,
+    levelTxt: "Mới chơi",
+    fullUrl: fullUrl,
+    title: "Mới chơi",
+    board: "",
+    langUrl: "/newbie"
+  });
+});
+
+router.get("/de", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/ai", {
+    bodyClass: "game",
+    level: 2,
+    levelTxt: "Dễ",
+    fullUrl: fullUrl,
+    title: "Dễ",
+    board: "",
+    langUrl: "/easy"
+  });
+});
+
+router.get("/binh-thuong", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/ai", {
+    bodyClass: "game",
+    level: 3,
+    levelTxt: "Bình thường",
+    fullUrl: fullUrl,
+    title: "Bình thường",
+    board: "",
+    langUrl: "/normal"
+  });
+});
+
+router.get("/kho", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/ai", {
+    bodyClass: "game",
+    level: 4,
+    levelTxt: "Khó",
+    fullUrl: fullUrl,
+    title: "Khó",
+    board: "",
+    langUrl: "/hard"
+  });
+});
+
+router.get("/cao-thu", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/ai", {
+    bodyClass: "game",
+    level: 5,
+    levelTxt: "Cao thủ",
+    fullUrl: fullUrl,
+    title: "Cao thủ",
+    board: "",
+    langUrl: "/insane"
+  });
+});
+
+router.get("/choi-voi-nhau", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/human", {
+    bodyClass: "game",
+    fullUrl: fullUrl,
+    title: "Chơi với nhau",
+    board: "",
+    langUrl: "/play-with-friend"
+  });
+});
+
+router.get("/xep-co", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/setup", {
+    bodyClass: "setup",
+    fullUrl: fullUrl,
+    title: "Xếp bàn cờ",
+    board: "",
+    langUrl: "/set-up"
+  });
+});
+
+router.get("/en", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/ai", {
+    bodyClass: "home",
     level: 3,
     levelTxt: "Normal",
     fullUrl: fullUrl,
     title: "Home",
-    board: ""
+    board: "",
+    langUrl: "/"
   });
 });
 
 router.get("/about", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/about", {
+  res.render("en/pages/about", {
     bodyClass: "about",
     fullUrl: fullUrl,
     title: "About",
-    board: ""
+    board: "",
+    langUrl: "/gioi-thieu"
   });
 });
 
 router.get("/newbie", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/ai", {
+  res.render("en/pages/ai", {
     bodyClass: "game",
     level: 1,
     levelTxt: "Newbie",
     fullUrl: fullUrl,
     title: "Newbie",
-    board: ""
+    board: "",
+    langUrl: "/moi-choi"
   });
 });
 
 router.get("/easy", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/ai", {
+  res.render("en/pages/ai", {
     bodyClass: "game",
     level: 2,
     levelTxt: "Easy",
     fullUrl: fullUrl,
     title: "Easy",
-    board: ""
+    board: "",
+    langUrl: "/de"
   });
 });
 
 router.get("/normal", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/ai", {
+  res.render("en/pages/ai", {
     bodyClass: "game",
     level: 3,
     levelTxt: "Normal",
     fullUrl: fullUrl,
     title: "Normal",
-    board: ""
+    board: "",
+    langUrl: "/binh-thuong"
   });
 });
 
 router.get("/hard", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/ai", {
+  res.render("en/pages/ai", {
     bodyClass: "game",
     level: 4,
     levelTxt: "Hard",
     fullUrl: fullUrl,
     title: "Hard",
-    board: ""
+    board: "",
+    langUrl: "/kho"
   });
 });
 
 router.get("/insane", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/ai", {
+  res.render("en/pages/ai", {
     bodyClass: "game",
     level: 5,
     levelTxt: "Insane",
     fullUrl: fullUrl,
     title: "Insane",
-    board: ""
+    board: "",
+    langUrl: "/cao-thu"
   });
 });
 
 router.get("/play-with-friend", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/human", {
+  res.render("en/pages/human", {
     bodyClass: "game",
     fullUrl: fullUrl,
     title: "Play with friend",
-    board: ""
+    board: "",
+    langUrl: "/choi-voi-nhau"
   });
 });
 
 router.get("/set-up", function(req, res){
   fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/setup", {
+  res.render("en/pages/setup", {
     bodyClass: "setup",
     fullUrl: fullUrl,
     title: "Setup board",
-    board: ""
+    board: "",
+    langUrl: "/xep-co"
   });
-});
-
-router.get("/chat", function(req, res){
-  fullUrl = "https://chessviet.com" + req.originalUrl;
-  res.render("pages/chat", {
-    bodyClass: "game",
-    fullUrl: fullUrl,
-    title: "Chat",
-    board: ""
-  })
 });
 
 module.exports = router;
