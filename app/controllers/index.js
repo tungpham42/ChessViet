455e6bea2256ebc65aa3a4677bf2fd9769790521
Tung Pham <tung.42@gmail.com>
2021-05-03 15:12:36 +0700

    REGEX integration

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -118,8 +118,20 @@ router.get("/xep-co", function(req, res){
     bodyClass: "setup",
     fullUrl: fullUrl,
     title: "Xếp bàn cờ",
-    board: "",
+    board: "8/8/8/8/8/8/8/8",
     langUrl: "/set-up"
+  });
+});
+
+router.get("/xep-co/:board([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/setup", {
+    bodyClass: "setup",
+    fullUrl: fullUrl,
+    title: "Xếp bàn cờ",
+    board: "",
+    langUrl: "/set-up/" + req.params.board,
+    board: req.params.board
   });
 });
 
@@ -229,8 +241,183 @@ router.get("/set-up", function(req, res){
     bodyClass: "setup",
     fullUrl: fullUrl,
     title: "Setup board",
-    board: "",
+    board: "8/8/8/8/8/8/8/8",
     langUrl: "/xep-co"
+  });
+});
+
+router.get("/set-up/:board([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/setup", {
+    bodyClass: "setup",
+    fullUrl: fullUrl,
+    title: "Setup board",
+    langUrl: "/xep-co/" + req.params.board,
+    board: req.params.board
+  });
+});
+
+router.get("/ban-co/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/board", {
+    bodyClass: "game",
+    fullUrl: fullUrl,
+    title: "Bàn cờ",
+    board: "",
+    langUrl: "/board/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/ban-co-moi-choi/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/boardAi", {
+    bodyClass: "game",
+    level: 1,
+    levelTxt: "Mới chơi",
+    fullUrl: fullUrl,
+    title: "Bàn cờ mới chơi",
+    board: "",
+    langUrl: "/newbie-board/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/ban-co-de/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/boardAi", {
+    bodyClass: "game",
+    level: 2,
+    levelTxt: "Dễ",
+    fullUrl: fullUrl,
+    title: "Bàn cờ dễ",
+    board: "",
+    langUrl: "/easy-board/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/ban-co-binh-thuong/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/boardAi", {
+    bodyClass: "game",
+    level: 3,
+    levelTxt: "Bình thường",
+    fullUrl: fullUrl,
+    title: "Bàn cờ bình thường",
+    board: "",
+    langUrl: "/normal-board/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/ban-co-kho/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/boardAi", {
+    bodyClass: "game",
+    level: 4,
+    levelTxt: "Khó",
+    fullUrl: fullUrl,
+    title: "Bàn cờ khó",
+    board: "",
+    langUrl: "/hard-board/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/ban-co-cao-thu/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("pages/boardAi", {
+    bodyClass: "game",
+    level: 5,
+    levelTxt: "Cao thủ",
+    fullUrl: fullUrl,
+    title: "Bàn cờ cao thủ",
+    board: "",
+    langUrl: "/insane-board/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/board/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/board", {
+    bodyClass: "game",
+    fullUrl: fullUrl,
+    title: "Board",
+    board: "",
+    langUrl: "/ban-co/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/newbie-board/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/boardAi", {
+    bodyClass: "game",
+    level: 1,
+    levelTxt: "Newbie",
+    fullUrl: fullUrl,
+    title: "Newbie board",
+    board: "",
+    langUrl: "/ban-co-moi-choi/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/easy-board/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/boardAi", {
+    bodyClass: "game",
+    level: 2,
+    levelTxt: "Easy",
+    fullUrl: fullUrl,
+    title: "Easy board",
+    board: "",
+    langUrl: "/ban-co-de/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/normal-board/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/boardAi", {
+    bodyClass: "game",
+    level: 3,
+    levelTxt: "Normal",
+    fullUrl: fullUrl,
+    title: "Normal board",
+    board: "",
+    langUrl: "/ban-co-binh-thuong/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/hard-board/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/boardAi", {
+    bodyClass: "game",
+    level: 4,
+    levelTxt: "Hard",
+    fullUrl: fullUrl,
+    title: "Hard board",
+    board: "",
+    langUrl: "/ban-co-kho/" + req.params.fen,
+    fen: req.params.fen
+  });
+});
+
+router.get("/insane-board/:fen([a-zA-Z0-9\-\/\s|%20|&nbsp;]+)", function(req, res){
+  fullUrl = "https://chessviet.com" + req.originalUrl;
+  res.render("en/pages/boardAi", {
+    bodyClass: "game",
+    level: 5,
+    levelTxt: "Insane",
+    fullUrl: fullUrl,
+    title: "Insane board",
+    board: "",
+    langUrl: "/ban-co-cao-thu/" + req.params.fen,
+    fen: req.params.fen
   });
 });
 
